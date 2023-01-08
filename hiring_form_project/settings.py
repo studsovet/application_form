7216aa79794748c58fd5be37b47b686a5fc00af7
Ivan Chekanov <is.2003.01@gmail.com>
2023-01-08 22:05:43 +0300

    :lock: Fix Django secret key access

--- hiring_form_project/settings.py
+++ hiring_form_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import utils
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0h4z2x^ifd9s09^posc%m=60=ak%r8n@krmqq1s5uxu11istha'
+SECRET_KEY = utils.get_config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
